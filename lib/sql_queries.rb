5f916e625c0878ca2f9e916e1f36f6f0292a8690
Shahwar Spall <shevy.spall@gmail.com>
2020-02-03 04:51:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, SUM(pledges.amount) FROM projects
   JOIN pledges ON projects.id = pledges.project_id
-  GROUP BY projects.category ORDER BY pledges.amount"
+  WHERE "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
